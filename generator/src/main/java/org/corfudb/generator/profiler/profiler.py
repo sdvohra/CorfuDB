eea7d68bb3c05320af7cca6bf62b907c00814603
Shriya Vohra <shriya.vohra@gmail.com>
2018-06-18 15:59:42 -0700

    Clean-up of all methods except table methods

--- generator/src/main/java/org/corfudb/generator/profiler/profiler.py
+++ generator/src/main/java/org/corfudb/generator/profiler/profiler.py
@@ -1,17 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-path = "/Users/vshriya/Desktop/current/CorfuDB/test/client-1529017385323.log"
+input_path = "/Users/vshriya/Desktop/current/CorfuDB/test/client-1529017385323.log"
 output_path = "results/shriya/"
-points = []
-all_ops = []
+points = []  # all data points, aka each entry in log files
+all_ops = []  # all operations mentioned in log files
 
 
 ### Helper Functions
 def setup():
-    global points
-    # open file and initialize vars
-    f = open(path)
+    global points, all_ops
+
+    # open file and populate global vars
+    f = open(input_path)
     for point in f.readlines():
         points.append(point)
         if get_event_name(point) not in all_ops:
@@ -19,6 +20,7 @@ def setup():
 
     # sort points by start time
     points.sort(key=lambda x: get_time_stamp(x))
+
 
 def get_time_stamp(entry):
     return int(entry.split()[0])
@@ -32,8 +34,15 @@ def get_event_name(entry):
     return entry.split()[3].split()[0]
 
 
-def is_event(entry, event):
-    return entry.split()[3].split()[0] == event
+def get_table_ID(entry):
+    if "[id]" not in entry:
+        raise TypeError("entry is not a table")
+    if "[id]" in entry:
+        return entry.split("[id] ")[1].split()[0]
+
+
+def get_duration(entry):
+    return int(entry.split("[dur] ")[1].split()[0])
 
 
 def nano_to_milli(time):
@@ -43,9 +52,11 @@ def nano_to_milli(time):
 ### Analysis
 def count_active_threads():
     '''
-    How many threads are active per millisecond?
+    Answers the query: how many threads are active per millisecond?
+    Creates a scatter plot
     '''
     print 1
+    # Count active threads per ms
     counts = {}  # ms interval --> set of active thread IDs
     for point in points:
         if int(nano_to_milli(get_time_stamp(point))) in counts:
@@ -53,7 +64,7 @@ def count_active_threads():
         else:
             counts[int(nano_to_milli(get_time_stamp(point)))] = set([get_thread_ID(point)])
 
-    # create scatter plot
+    # Create scatter plot
     x = []
     y = []
     for key in counts.keys():
@@ -61,18 +72,20 @@ def count_active_threads():
         y += [len(counts[key])]
 
     plt.scatter(x, y)
-    plt.title("# of Active Threads vs. Time Elapsed")
+    plt.title("Number of Active Threads vs. Time Elapsed")
     plt.xlabel("Time Elapsed (ms)")
-    plt.ylabel("Number of Unique Active Threads")
+    plt.ylabel("Number of Active Threads")
     plt.savefig(output_path + "count_active_threads.png", bbox_inches='tight')
     plt.clf()
 
 
 def count_seq_calls():
     '''
-    How many sequencer calls are made per millisecond?
+    Answers the query: how many sequencer calls are made per millisecond?
+    Creates a scatter plot
     '''
-    print 3
+    print 2
+    # Count sequencer calls per ms
     counts = {}  # ms interval --> counts of seq calls
     for point in points:
         if get_event_name(point) == "Seq":
@@ -81,7 +94,7 @@ def count_seq_calls():
             else:
                 counts[int(nano_to_milli(get_time_stamp(point)))] = 1
 
-    # create scatter plot
+    # Create scatter plot
     x = []
     y = []
     for key in counts.keys():
@@ -89,114 +102,42 @@ def count_seq_calls():
         y += [counts[key]]
 
     plt.scatter(x, y)
-    plt.title("# of Seq Calls vs. Time Elapsed")
+    plt.title("Number of Seq Calls vs. Time Elapsed")
     plt.xlabel("Time Elapsed (ms)")
     plt.ylabel("Number of Seq Calls")
     plt.savefig(output_path + "count_seq_calls.png", bbox_inches='tight')
     plt.clf()
 
 
-def count_stepping():
-    print 4
-    counts = {}  # ms interval --> count of followBackpointers
-    for point in points:
-        if get_event_name(point) == "followBackpointers":
-            if int(nano_to_milli(get_time_stamp(point))) in counts:
-                counts[int(nano_to_milli(get_time_stamp(point)))] += 1
-            else:
-                counts[int(nano_to_milli(get_time_stamp(point)))] = 1
-
-    # create scatter plot
-    x = []
-    y = []
-    for key in counts.keys():
-        x += [key - min(counts.keys())]
-        y += [counts[key]]
-
-    plt.scatter(x, y)
-    plt.title("# of followBackpointers vs. Time Elapsed")
-    plt.xlabel("Time Elapsed (ms)")
-    plt.ylabel("Number of followBackpointers")
-    plt.savefig(output_path + "count_stepping.png", bbox_inches='tight')
-    plt.clf()
-
-
-def count_unsafe():
-    print 5
-    counts1 = {}  # ms interval --> count of rollbackObjectUnsafe
-    for point in points:
-        if get_event_name(point) == "rollbackObjectUnsafe":
-            if int(nano_to_milli(get_time_stamp(point))) in counts1:
-                counts1[int(nano_to_milli(get_time_stamp(point)))] += 1
-            else:
-                counts1[int(nano_to_milli(get_time_stamp(point)))] = 1
-
-    counts2 = {}  # ms interval --> count of syncObjectUnsafe
-    for point in points:
-        if get_event_name(point) == "syncObjectUnsafe":
-            if int(nano_to_milli(get_time_stamp(point))) in counts2:
-                counts2[int(nano_to_milli(get_time_stamp(point)))] += 1
-            else:
-                counts2[int(nano_to_milli(get_time_stamp(point)))] = 1
-
-    # create juxtaposed scatter plot
-    x1 = []
-    y1 = []
-    for key in counts1.keys():
-        x1 += [key - min(counts1.keys())]
-        y1 += [counts1[key]]
-    plt.scatter(x1, y1, s=3, color='green', alpha=0.5, label="rollbackObjUnsafe")
-
-    x2 = []
-    y2 = []
-    for key in counts2.keys():
-        x2 += [key - min(counts2.keys())]
-        y2 += [counts2[key]]
-    plt.scatter(x2, y2, s=3, alpha=0.5, label="syncObjUnsafe")
-
-    plt.title("Comparison")
-    plt.xlabel("Time Elapsed (ms)")
-    plt.ylabel("#")
-    plt.legend(loc=1)
-    plt.savefig(output_path + "count_unsafe.png", bbox_inches='tight')
-    plt.clf()
-
-
 def count_ops_per_tx():
-    print 6
-    counts = {}  # thread num --> count of ops
-    num_ops = []
-    i = 0
+    '''
+    Answers the query: how many operations are performed per transaction?
+    Creates a bar chart
+    '''
+    print 3
+    # Count number of ops in every transaction
+    counts = {}  # thread num --> count of ops (in one tx in that thread)
+    num_ops = []  # list of number of ops taken by each tx
     for point in points:
-        if get_thread_ID(point) in counts and get_event_name(point) != "TXBegin" and get_event_name(point) != "TXEnd":
-            counts[get_thread_ID(point)] += 1
-        elif get_event_name(point) == "TXBegin":
+        if get_event_name(point) == "TXBegin":
             counts[get_thread_ID(point)] = 0
         elif get_event_name(point) == "TXEnd":
-            i += 1
             num_ops += [counts[get_thread_ID(point)]]
             counts[get_thread_ID(point)] = 0
+        elif get_thread_ID(point) in counts:
+            counts[get_thread_ID(point)] += 1
 
-    print i
-    # create 1-D scatter plot
-    y = num_ops
-    x = [0 for _ in y]
-    plt.scatter(x, y, alpha=0.3)
-    plt.title("Num Ops per Transaction")
-    plt.ylabel("Num Ops")
-    plt.savefig(output_path + "count_ops_per_tx_scatter.png", bbox_inches='tight')
-    plt.clf()
-
-    # create bar chart
-    num_num_ops = {} # number --> number of times that number appears
+    # Count frequency of each number of ops
+    num_num_ops = {}  # number --> number of times that number appears
     for n in num_ops:
         if n not in num_num_ops:
             num_num_ops[n] = 1
         else:
             num_num_ops[n] += 1
-
-    y = num_num_ops.values()
     x = num_num_ops.keys()
+    y = num_num_ops.values()
+
+    # Create bar chart
     width = 1 / 1.5
     plt.bar(x, y, width, color="blue")
     plt.title("Num Ops per Transaction")
@@ -207,7 +148,7 @@ def count_ops_per_tx():
 
 
 def count_table_ops_per_tx():
-    print 7
+    print 4
     counts_access = {} # thread num --> count of access ops
     counts_mutate = {} # thread num --> count of mutate ops
     num_access_ops = []
@@ -278,7 +219,7 @@ def count_table_ops_per_tx():
 
 
 def count_id_table_ops_per_tx(table_id):
-    print 8
+    print 5
     counts_access = {} # thread num --> count of access ops
     counts_mutate = {} # thread num --> count of mutate ops
     num_access_ops = []
@@ -351,152 +292,140 @@ def count_id_table_ops_per_tx(table_id):
 
 
 def count_all_ops():
-    print 9
-    counts = {} # op name --> count
+    '''
+    Answers the query: how many times was each operation called w.r.t. the total number of calls?
+    Creates a pie chart
+    '''
+    print 6
+    # Count number of times each operation was called
+    counts = {}  # op name --> count
     for point in points:
         if get_event_name(point) not in counts:
             counts[get_event_name(point)] = 1
         else:
             counts[get_event_name(point)] += 1
 
-    # create pie chart
-    sizes = []
-    labels = []
-    for op in counts.keys():
-        labels += [op]
-        sizes += [counts[op]]
-    plt.pie(sizes, labels=labels, colors=['gold', 'yellowgreen', 'lightcoral', 'lightskyblue'], autopct='%1.1f%%')
+    # Create pie chart
+    plt.pie(counts.values(), labels=counts.keys(), colors=['gold', 'yellowgreen', 'lightcoral', 'lightskyblue'],
+            autopct='%1.1f%%')
     plt.axis('equal')
     plt.savefig(output_path + "count_all_ops.png", bbox_inches='tight')
     plt.clf()
 
 
-def count_all_ops_time(): # review this code - may not be 100% accurate, esp. for TXBegin portion!
-    print 10
-    last_ts = {} # thread # --> last time stamp
-    time_count = {} # thread # --> {op name --> cumulative time taken so far}
-
+def count_all_ops_time():
+    '''
+    Answers the query: how much time did each operation take w.r.t. the total runtime?
+    Creates a pie chart
+    '''
+    print 7
+    # Count total time taken by each op
+    # NOT CORRECT - double counts things in threads!!!
+    counts = {}  # op name --> total time taken by this op
     for point in points:
-        if get_thread_ID(point) not in last_ts: # double check this edge case
-            last_ts[get_thread_ID(point)] = get_time_stamp(point)
-        if get_thread_ID(point) not in time_count:
-            time_count[get_thread_ID(point)] = {}
-        if get_event_name(point) not in time_count[get_thread_ID(point)]:
-            time_count[get_thread_ID(point)][get_event_name(point)] = 0
+        if get_event_name(point) not in counts:
+            counts[get_event_name(point)] = get_duration(point)
+        else:
+            counts[get_event_name(point)] += get_duration(point)
 
-        time_count[get_thread_ID(point)][get_event_name(point)] += get_time_stamp(point) - last_ts[get_thread_ID(point)]
-        last_ts[get_thread_ID(point)] = get_time_stamp(point)
-
-    # create pie chart
-    sizes = []
-    labels = []
-    sum_time_count = {} # op name --> total time for op across all threads
-    for thread in time_count.keys():
-        for op in time_count[thread].keys():
-            if op not in sum_time_count:
-                sum_time_count[op] = time_count[thread][op]
-            else:
-                sum_time_count[op] += time_count[thread][op]
-    for op in sum_time_count.keys():
-        labels += [op]
-        sizes += [sum_time_count[op]]
-    plt.pie(sizes, labels=labels, colors=['gold', 'yellowgreen', 'lightcoral', 'lightskyblue'], autopct='%1.1f%%')
+    # Create pie chart
+    plt.pie(counts.values(), labels=counts.keys(), colors=['gold', 'yellowgreen', 'lightcoral', 'lightskyblue'],
+            autopct='%1.1f%%')
     plt.axis('equal')
     plt.savefig(output_path + "count_all_ops_time.png", bbox_inches='tight')
     plt.clf()
 
 
 def count_all_ops_time_by_tx():
-    print 11
-
-    global all_ops
-    last_ts = {} # thread # --> last time stamp
-    in_tx = {} # thread # --> (bool) currently in tx
-    time_count = {} # thread # --> {op name --> cumulative time taken so far}
-
-    txs = [] # list of lists, txs[i] = np.array of how much time each op takes in tx i
-
+    '''
+    Answers the query: how much time did each transactional operation take on average across all transactions
+    w.r.t. the total runtime of all transactions?
+    Creates a pie chart
+    '''
+    print 8
+    # Count total time taken by each op w/i each tx
+    # NOT CORRECT for same reason as above!
+    txs = []  # list of lists, txs[i] = np.array of how much time each op takes in tx i
+    in_tx = {}  # thread # --> (bool) currently in tx
+    counts = {}  # thread # --> {op name --> total time taken by this op}
     for point in points:
         if get_event_name(point) == "TXBegin":
             in_tx[get_thread_ID(point)] = True
 
         # only consider ops that are part of a tx
-        if get_thread_ID(point) in in_tx and in_tx[get_thread_ID(point)] == True:
-            if get_thread_ID(point) not in last_ts: # double check this edge case
-                last_ts[get_thread_ID(point)] = get_time_stamp(point)
-            if get_thread_ID(point) not in time_count:
-                time_count[get_thread_ID(point)] = {}
-            if get_event_name(point) not in time_count[get_thread_ID(point)]:
-                time_count[get_thread_ID(point)][get_event_name(point)] = 0
-            time_count[get_thread_ID(point)][get_event_name(point)] += get_time_stamp(point) - last_ts[get_thread_ID(point)]
+        if get_thread_ID(point) in in_tx and in_tx[get_thread_ID(point)]:
+            if get_event_name(point) not in counts:
+                counts[get_thread_ID(point)][get_event_name(point)] = get_duration(point)
+            else:
+                counts[get_thread_ID(point)][get_event_name(point)] += get_duration(point)
 
         if get_event_name(point) == "TXEnd":
-            # append info in time_count to final np.array for bar chart based on all_ops and indexing
+            # append info in counts to txs
             in_tx[get_thread_ID(point)] = False
             tx = np.zeros(len(all_ops))
             for i in range(len(all_ops)):
-                if all_ops[i] in time_count[get_thread_ID(point)]:
-                    tx[i] = time_count[get_thread_ID(point)][all_ops[i]]
+                if all_ops[i] in counts[get_thread_ID(point)]:
+                    tx[i] = counts[get_thread_ID(point)][all_ops[i]]
             txs += [tx]
             # clear time_count for that thread
-            del time_count[get_thread_ID(point)]
+            del counts[get_thread_ID(point)]
 
-        # still increment time stamp
-        last_ts[get_thread_ID(point)] = get_time_stamp(point)
-
-    # create stacked bar chart
     tx_mtx = np.array([tx for tx in txs])
-    tx_mtx_T = tx_mtx.T
+    tx_mtx_T = tx_mtx.T  # aggregates counts s.t. each op has its own list
 
-    avg_vals = []
+    avg_vals = []  # list of avg time taken by each op
     for op in tx_mtx_T:
         avg_vals += [np.average(op)]
 
     # create pie chart
-    sizes = avg_vals
-    labels = all_ops
-    plt.pie(sizes, labels=labels, colors=['gold', 'yellowgreen', 'lightcoral', 'lightskyblue'], autopct='%1.1f%%')
+    plt.pie(avg_vals, labels=all_ops, colors=['gold', 'yellowgreen', 'lightcoral', 'lightskyblue'], autopct='%1.1f%%')
     plt.axis('equal')
     plt.savefig(output_path + "count_all_ops_time_by_tx.png", bbox_inches='tight')
     plt.clf()
 
 
 def count_reads():
-    print 12
-    counts1 = {}  # ms interval --> count of tx reads
+    '''
+    Answers the query: how many reads (both tx and non-tx) are done per millisecond?
+    Creates a scatter plot, with both tx reads and non-tx reads
+    '''
+    print 9
+    # Count tx reads
+    counts_tx = {}  # ms interval --> count of tx reads
     for point in points:
         if get_event_name(point) == "access(tx)":
-            if int(nano_to_milli(get_time_stamp(point))) in counts1:
-                counts1[int(nano_to_milli(get_time_stamp(point)))] += 1
+            if int(nano_to_milli(get_time_stamp(point))) in counts_tx:
+                counts_tx[int(nano_to_milli(get_time_stamp(point)))] += 1
             else:
-                counts1[int(nano_to_milli(get_time_stamp(point)))] = 1
+                counts_tx[int(nano_to_milli(get_time_stamp(point)))] = 1
 
-    counts2 = {}  # ms interval --> count of non tx reads
+    # Count non-tx reads
+    counts_non_tx = {}  # ms interval --> count of non-tx reads
     for point in points:
         if get_event_name(point) == "access(nonTx)":
-            if int(nano_to_milli(get_time_stamp(point))) in counts2:
-                counts2[int(nano_to_milli(get_time_stamp(point)))] += 1
+            if int(nano_to_milli(get_time_stamp(point))) in counts_non_tx:
+                counts_non_tx[int(nano_to_milli(get_time_stamp(point)))] += 1
             else:
-                counts2[int(nano_to_milli(get_time_stamp(point)))] = 1
+                counts_non_tx[int(nano_to_milli(get_time_stamp(point)))] = 1
 
-    # create juxtaposed scatter plot
-    x1 = []
-    y1 = []
-    for key in counts1.keys():
-        x1 += [key - min(counts1.keys())]
-        y1 += [counts1[key]]
-    plt.scatter(x1, y1, color='green', label="tx reads")
+    # Create juxtaposed scatter plot
+    x_tx = []
+    y_tx = []
+    for key in counts_tx.keys():
+        x_tx += [key - min(counts_tx.keys())]
+        y_tx += [counts_tx[key]]
+    plt.scatter(x_tx, y_tx, color='green', label="tx reads")
 
-    x2 = []
-    y2 = []
-    for key in counts2.keys():
-        x2 += [key - min(counts2.keys())]
-        y2 += [counts2[key]]
-    plt.scatter(x2, y2, label="non tx reads")
+    x_non_tx = []
+    y_non_tx = []
+    for key in counts_non_tx.keys():
+        x_non_tx += [key - min(counts_non_tx.keys())]
+        y_non_tx += [counts_non_tx[key]]
+    plt.scatter(x_non_tx, y_non_tx, label="non-tx reads")
 
-    plt.title("Comparison")
+    plt.title("Tx and Non-Tx Reads vs. Time")
     plt.xlabel("Time Elapsed (ms)")
-    plt.ylabel("#")
+    plt.ylabel("Number of Reads")
     plt.legend(loc=1)
     plt.savefig(output_path + "count_reads_scatter.png", bbox_inches='tight')
     plt.clf()
@@ -506,8 +435,6 @@ def count_reads():
 setup()
 count_active_threads()
 count_seq_calls()
-count_stepping()
-count_unsafe()
 count_ops_per_tx()
 count_table_ops_per_tx()
 count_id_table_ops_per_tx("7c4f2940-7893-3334-a6cb-7a87bf045c0d")
